db2cb57cbe7d17d688adf10bbf0b539e5e87f711
cloudspawn <102333418+cloudspawn@users.noreply.github.com>
2022-03-25 01:48:16 +0100

    Update nbCach.py

--- nbCach.py
+++ nbCach.py
@@ -1,12 +1,14 @@
 import random
 
 print("*********************Nombre Secret**********************")
+#generation du nombre aleatoire
 secret=random.randint(1,101)
-print(secret)
 print("********************************************************")
+
 flag=False
 compteur=0
 
+#tant que le nombre n'a pas ete trouvé, boucle flag False
 while flag==False:
     choix=int(input("Entrez un nombre entre 0 et 100\n"))
     compteur+=1
@@ -16,4 +18,6 @@ while flag==False:
         print("Trop petit!!")
     elif choix>secret:
         print("Trop grand!!")
+
+#le nombre a été trouvé, le flag est True on sort de la boucle
 print("Bravo le nombre etait "+str(secret)+"\n coups pour trouver:",compteur)
